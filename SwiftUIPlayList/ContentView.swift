0d376de24f74c808b91b45c322bf68121cc48d2a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-29 14:27:39 +0300

    Initial Commit

---
+++ SwiftUIPlayList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayList
+//
+//  Created by ramil on 29.06.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPlayList/SwiftUIPlayListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayListApp.swift
+//  SwiftUIPlayList
+//
+//  Created by ramil on 29.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
